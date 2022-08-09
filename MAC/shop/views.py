f6d64b7663676262995fd65e263695f8736fa6aa
Manish Pingale <manishpingale2005@gmail.com>
2022-08-09 10:25:33 +0530

    Fetcheds products and displayed

--- MAC/shop/views.py
+++ MAC/shop/views.py
@@ -1,23 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
 
 def index(request):
-    return render(request, 'shop/index.html')
+    fetchedProducts = Product.objects.all()
+    n = len(fetchedProducts)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {
+        "noOfSlides": nSlides,
+        'range': range(1,nSlides),
+        "products": fetchedProducts
+    }
+    return render(request, 'shop/index.html', params)
+
 
 def search(request):
     return HttpResponse("At Search")
 
+
 def about(request):
-    return HttpResponse("At About")
+    return render(request, 'shop/about.html')
+
 
 def contact(request):
     return HttpResponse("At Contact")
 
+
 def productView(request):
     return HttpResponse("At Product View")
 
+
 def tracker(request):
     return HttpResponse("At Tracker")
+
 
 def checkout(request):
     return HttpResponse("At Checkout")
