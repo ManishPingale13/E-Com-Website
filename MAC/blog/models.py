e70d26afa6b3a4d2503f853d31b8207241abe656
Manish Pingale <manishpingale2005@gmail.com>
2022-08-16 11:12:29 +0530

    Most of the blog done

--- MAC/blog/models.py
+++ MAC/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogpost(models.Model):
+    postId = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    head0 = models.CharField(max_length=500,default="")
+    head1 = models.CharField(max_length=500,default="")
+    head2 = models.CharField(max_length=500,default="")
+    cHead0 = models.CharField(max_length=500,default="")
+    cHead1 = models.CharField(max_length=500,default="")
+    cHead2 = models.CharField(max_length=500,default="")
+    thumbnail = models.ImageField(upload_to="shop/images",default="")
+    publishDate = models.DateField()
+
+    def __str__(self):
+        return self.title
--- MAC/blog/views.py
+++ MAC/blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blogpost
+
 
 def index(request):
-    return render(request, 'blog/index.html')
+    blogs = Blogpost.objects.all()
+    print(blogs)
+    return render(request, 'blog/index.html', {'blogs': blogs})
+
+
+def blogpost(request, id):
+    post = Blogpost.objects.filter(postId=id)[0]
+    return render(request, 'blog/blogpost.html', {'post': post})
