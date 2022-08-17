28e6d4a3df90442fd628272b7afd5e7b140e9f65
Manish Pingale <manishpingale2005@gmail.com>
2022-08-17 12:40:40 +0530

    WTF is going on!!!!!!!!!!!!

--- MAC/MAC/settings.py
+++ MAC/MAC/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-aw0j2$fazft^davk*1*n8(18*s0@=x#jbj7e$*4u3qy!y%a935'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
